437f96377e826f5df1c3b80979d5f502d7f44c1e
suraviNtt <93370272+suraviNtt@users.noreply.github.com>
2021-11-23 11:41:24 +0530

    Commit msg

--- NewTours/src/main/java/com/guru99/demo/pages/SearchDeatilsPage.java
+++ NewTours/src/main/java/com/guru99/demo/pages/SearchDeatilsPage.java
@@ -1,5 +1,27 @@
 package com.guru99.demo.pages;
 
-public class SearchDeatilsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.guru99.demo.utility.CommonMethods;
+
+public class SearchDeatilsPage  extends CommonMethods {
+
+	@FindBy(id = "email")
+	WebElement emailInbox;
+	
+	@FindBy(xpath = "//input[@name='password']")
+	WebElement passwordField;
+	
+	@FindBy(xpath = "//input[@name='confirmPassword']")
+	WebElement confrmPasswordField;
+	
+	@FindBy(xpath = "//input[@name='submit']")
+	WebElement submit;
+	
+	public SearchDeatilsPage() {
+		PageFactory.initElements(driver, this);
+	}
 
 }
