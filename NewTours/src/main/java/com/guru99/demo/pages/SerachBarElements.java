565be430412f206461596faa19c0f1577440a0d4
suraviNtt <93370272+suraviNtt@users.noreply.github.com>
2021-11-23 11:46:33 +0530

    git changes update

--- NewTours/src/main/java/com/guru99/demo/pages/SerachBarElements.java
+++ NewTours/src/main/java/com/guru99/demo/pages/SerachBarElements.java
@@ -1,5 +1,26 @@
 package com.guru99.demo.pages;
 
-public class SerachBarElements {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.guru99.demo.utility.CommonMethods;
+
+public class SerachBarElements extends CommonMethods {
+
+	@FindBy(id = "email")
+	WebElement emailInbox;
+
+	@FindBy(xpath = "//input[@name='password']")
+	WebElement passwordField;
+
+	@FindBy(xpath = "//input[@name='confirmPassword']")
+	WebElement confrmPasswordField;
+
+	@FindBy(xpath = "//input[@name='submit']")
+	WebElement submit;
+
+	public SerachBarElements() {
+		PageFactory.initElements(driver, this);
+	}
 }
